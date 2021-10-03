dd97ed92a2175b9f861437d3aa1bc460197919d8
Kevin Miguta <migutak@gmail.com>
2021-10-04 00:19:26 +0300

    data

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 module.exports = {
     filePath: process.env.FILEPATH || '/home/ecollectadmin/demandletters/', //'/app/nfs/demandletters/'
     //imagePath: process.env.IMAGEPATH || '/home/ecollectadmin/docxletters/routes/',
-    smtpserver: process.env.SMTPSERVER || 'office365.officer',
+    smtpserver: process.env.SMTPSERVER || 'smtp.gmail.com',//'office365.officer',
     smtpport: process.env.SMTPPORT || 587,
-    user: process.env.USER || 'ecollect',
-    pass:  process.env.PASS || ''
+    user: process.env.USER || 'ecollectsystem',
+    pass:  process.env.PASS || 'W1ndowsxp'
 }
